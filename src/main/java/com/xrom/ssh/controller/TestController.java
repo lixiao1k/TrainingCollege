9f8f00e8f95e1d1890ec14373482805e05e4745d
李晓冬 <lixiao1k@163.com>
2018-03-08 16:49:02 +0800

    课程相关业务逻辑测试完成

--- src/main/java/com/xrom/ssh/controller/TestController.java
+++ src/main/java/com/xrom/ssh/controller/TestController.java
@@ -63,28 +63,102 @@ public class TestController {
     }
 
 
-    @RequestMapping(value = "/createTeacher", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
+    @RequestMapping(value = "/createCourse", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
     @ResponseBody
-    public String createTeacher(){
-        teacherService.createTeacher("Mike", "127799495", "Maths", "96eda85");
+    public String createCourse(){
+        courseService.createCourse(new Date(1000), new Date(2000) , "Chinese",
+                "Good for you chinese", 6, 4, "cf01a9c", 57);
         return "success";
     }
 
-    @RequestMapping(value = "/delete", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
+    @RequestMapping(value = "/getCourse", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
     @ResponseBody
-    public String delete(){
-        teacherService.deleteTeacher(3L);
-        return "success";
-    }
-
-    @RequestMapping(value = "/getAll", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
-    @ResponseBody
-    public String getAll(){
-        List<Teacher> teachers = teacherService.findTeachersOfInstitution("cf01a9c");
-        for (Teacher teacher : teachers){
-            System.out.println(teacher);
+    public String getCourse(){
+        List<Course> courses = courseService.findAll();
+        for(Course course : courses){
+            System.out.println(course);
         }
         return "success";
     }
 
+    @RequestMapping(value = "/getCourse1", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
+    @ResponseBody
+    public String getCourse1(){
+        List<Course> courses = courseService.findAll("1234567");
+        for(Course course : courses){
+            System.out.println(course);
+        }
+        return "success";
+    }
+
+    @RequestMapping(value = "/getCourse2", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
+    @ResponseBody
+    public String getCourse2(){
+        List<Course> courses = courseService.findAll(true);
+        for(Course course : courses){
+            System.out.println(course);
+        }
+        return "success";
+    }
+
+    @RequestMapping(value = "/getCourse3", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
+    @ResponseBody
+    public String getCourse3(){
+        List<Course> courses = courseService.findAll(false);
+        for(Course course : courses){
+            System.out.println(course);
+        }
+        return "success";
+    }
+
+    @RequestMapping(value = "/getCourse4", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
+    @ResponseBody
+    public String getCourse4(){
+        List<Course> courses = courseService.findAll("1234567", true);
+        for(Course course : courses){
+            System.out.println(course);
+        }
+        return "success";
+    }
+
+    @RequestMapping(value = "/getCourse5", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
+    @ResponseBody
+    public String getCourse5(){
+        Course course = courseService.getCourse(1L);
+        if (course == null){
+            return "WU";
+        }else {
+            System.out.println(course);
+        }
+        return "Success!";
+    }
+
+    @RequestMapping(value = "/getCourse6", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
+    @ResponseBody
+    public String getCourse6(){
+        Course course = courseService.getCourse(0L);
+        if (course == null){
+            return "WU";
+        }else {
+            System.out.println(course);
+        }
+        return "Success!";
+    }
+
+    @RequestMapping(value = "/deleteCourse", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
+    @ResponseBody
+    public String delete(){
+        courseService.deleteCourse(1L);
+        return "Success!";
+    }
+
+    @RequestMapping(value = "/getAllByType", method = RequestMethod.GET, produces="text/html;charset=UTF-8")
+    @ResponseBody
+    public String byType(){
+        List<Course> courses = courseService.findAllByType("Chinese");
+        for(Course course : courses){
+            System.out.println(course);
+        }
+        return "Success!";
+    }
 }
